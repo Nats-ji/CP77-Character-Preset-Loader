d2c092fab0de1ec0516192307b93bcad51626294
Nats-ji <angel_tommy@foxmail.com>
2021-02-12 12:40:36 +0800

    Set window posistion for firstuse

--- init.lua
+++ init.lua
@@ -2,11 +2,13 @@ registerForEvent("onInit", function()
   CPS = require("CPStyling")
   PresetSystem = require("PresetSystem")
   UISystem = require("UISystem")
+  wWidth, wHeight = GetDisplayResolution()
 end)
 
 registerForEvent("onDraw", function()
 
   if drawWindow then
+    ImGui.SetNextWindowPos(wWidth/2, wHeight/2, ImGuiCond.FirstUseEver)
     CPS.setThemeBegin()
     ImGui.Begin("Character Preset Loader", ImGuiWindowFlags.NoResize | ImGuiWindowFlags.AlwaysAutoResize)
     CPS.setFrameThemeBegin()
